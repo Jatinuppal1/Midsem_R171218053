6390a8205a59151846964adfc6401b6426ee59dc
Jatin Uppal <jatinuppal89@gmail.com>
2019-03-18 11:11:33 +0530

    all definitions are provided.

--- data_entry.cpp
+++ data_entry.cpp
@@ -4,10 +4,26 @@ class data
 {
 	string name;
 	int roll_no;
+public:
 	void dat_entry();
 	void display();
+};
+void data::dat_entry()
+{
+	cout << "\nEnter your namr - ";
+	cin >> name;
+	cout << "\nEnter your roll no. - ";
+	cin >> roll_no;
+	display();
+}
+void data::display()
+{
+	cout<<"\nYour name is "<<name;
+	cout<<"\nYour roll no. is "<<roll_no;
 }
 int main()
 {
+	data D;
+	D.dat_entry();
 	return 0;
 }
